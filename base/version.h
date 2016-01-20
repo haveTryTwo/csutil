a4f763e4d80b5fb7a9f4d572cbdcfeae1bf052a8
liuzhongchao <liuzhongchao197@163.com>
2016-01-20 19:25:58 +0800

    1)Adding: add base version

---
+++ base/version.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2015 The CCUtil Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_VERSION_H_
+#define BASE_VERSION_H_
+
+namespace base
+{
+
+const std::string kBaseVerson("1.0.0");
+
+inline std::string GetBaseVersion()
+{
+    return kBaseVerson;
+}
+
+}
+
+#endif
