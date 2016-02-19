c4fded77531bea441a7784f33d4b865c21bb68fa
liuzhongchao <liuzhongchao197@163.com>
2016-02-19 17:55:17 +0800

    1)Addding: index binary search for stl

--- base/search.cc
+++ base/search.cc
@@ -5,39 +5,46 @@ namespace base
 }
 
 #ifdef _SEARCH_MAIN_TEST_
+#include <deque>
+#include <vector>
+#include <utility>
+
 #include <assert.h>
 
 int main(int argc, char *argv[])
-{
+{/*{{{*/
     using namespace base;
 
     // descend search
-    int arr_odd[] = {40, 35, 30, 25, 20, 15, 10};
+    int arr_des_odd[] = {40, 35, 30, 25, 20, 15, 10};
+    Print(arr_des_odd, arr_des_odd + sizeof(arr_des_odd)/sizeof(arr_des_odd[0]), arr_des_odd[0]);
     int pos = 0;
     int test_arr_odd[] = {43, 40, 37, 35, 32, 30, 28, 25, 21, 20, 16, 15, 13, 10, 2 };
     for (int i = 0; i < (int)(sizeof(test_arr_odd)/sizeof(test_arr_odd[0])); ++i)
     {
-        Code ret = BinaryDescendSearch(arr_odd, sizeof(arr_odd)/sizeof(arr_odd[0]),
+        Code ret = BinaryDescendSearch(arr_des_odd, sizeof(arr_des_odd)/sizeof(arr_des_odd[0]),
                                 test_arr_odd[i], &pos, CompareNum<int>);
         assert(ret == kOk);
-        fprintf(stderr, "test_value:%d, find pos:%d, value:%d\n", test_arr_odd[i], pos, arr_odd[pos]);
+        fprintf(stderr, "test_value:%d, find pos:%d, value:%d\n", test_arr_odd[i], pos, arr_des_odd[pos]);
     }
     fprintf(stderr, "\n");
 
-    int arr_even[] = {40, 35, 30, 25, 20, 15};
+    int arr_des_even[] = {40, 35, 30, 25, 20, 15};
+    Print(arr_des_even, arr_des_even + sizeof(arr_des_even)/sizeof(arr_des_even[0]), arr_des_even[0]);
     int test_arr_even[] = {44, 40, 38, 35, 33, 30, 26, 25, 24, 20, 18, 15, 11};
     for (int i = 0; i < (int)(sizeof(test_arr_even)/sizeof(test_arr_even[0])); ++i)
     {
-        Code ret = BinaryDescendSearch(arr_even, sizeof(arr_even)/sizeof(arr_even[0]),
+        Code ret = BinaryDescendSearch(arr_des_even, sizeof(arr_des_even)/sizeof(arr_des_even[0]),
                                 test_arr_even[i], &pos, CompareNum<int>);
         assert(ret == kOk);
-        fprintf(stderr, "test_value:%d, find pos:%d, value:%d\n", test_arr_even[i], pos, arr_even[pos]);
+        fprintf(stderr, "test_value:%d, find pos:%d, value:%d\n", test_arr_even[i], pos, arr_des_even[pos]);
     }
-
     fprintf(stderr, "\n");
+
     // ascend search
     int arr_asc_odd[] = {10, 15, 20, 25, 30, 35, 40};
-    for (int i = 0; i < (int)(sizeof(test_arr_odd)/sizeof(test_arr_odd[0])); ++i)
+    Print(arr_asc_odd, arr_asc_odd + sizeof(arr_asc_odd)/sizeof(arr_asc_odd[0]), arr_asc_odd[0]);
+    for (int i = (int)(sizeof(test_arr_odd)/sizeof(test_arr_odd[0])) - 1; i >= 0; --i)
     {
         Code ret = BinaryAscendSearch(arr_asc_odd, sizeof(arr_asc_odd)/sizeof(arr_asc_odd[0]),
                                 test_arr_odd[i], &pos, CompareNum<int>);
@@ -47,14 +54,100 @@ int main(int argc, char *argv[])
     fprintf(stderr, "\n");
 
     int arr_asc_even[] = {15, 20, 25, 30, 35, 40};
-    for (int i = 0; i < (int)(sizeof(test_arr_even)/sizeof(test_arr_even[0])); ++i)
+    Print(arr_asc_even, arr_asc_even + sizeof(arr_asc_even)/sizeof(arr_asc_even[0]), arr_asc_even[0]);
+    for (int i = (int)(sizeof(test_arr_even)/sizeof(test_arr_even[0])) -1; i >= 0; --i)
     {
         Code ret = BinaryAscendSearch(arr_asc_even, sizeof(arr_asc_even)/sizeof(arr_asc_even[0]),
                                 test_arr_even[i], &pos, CompareNum<int>);
         assert(ret == kOk);
         fprintf(stderr, "test_value:%d, find pos:%d, value:%d\n", test_arr_even[i], pos, arr_asc_even[pos]);
     }
+    fprintf(stderr, "\n");
+
+    // descend search using RandomIterator
+    std::vector<int> vec_des_odd(arr_des_odd, arr_des_odd+sizeof(arr_des_odd)/sizeof(arr_des_odd[0]));
+    Print(vec_des_odd.begin(), vec_des_odd.end(), vec_des_odd[0]);
+    std::vector<int>::iterator it;
+    for (int i = 0; i < (int)(sizeof(test_arr_odd)/sizeof(test_arr_odd[0])); ++i)
+    {
+        Code ret = BinaryDescendSearch(vec_des_odd.begin(), vec_des_odd.end(),
+                    test_arr_odd[i], &it, CompareNum<int>);
+        assert(ret == kOk);
+        fprintf(stderr, "test_value:%d, value:%d\n", test_arr_odd[i], *it);
+    }
+    fprintf(stderr, "\n");
+
+    std::vector<int> vec_des_even(arr_des_even, arr_des_even+sizeof(arr_des_even)/sizeof(arr_des_even[0]));
+    Print(vec_des_even.begin(), vec_des_even.end(), vec_des_even[0]);
+    for (int i = 0; i < (int)(sizeof(test_arr_even)/sizeof(test_arr_even[0])); ++i)
+    {
+        Code ret = BinaryDescendSearch(vec_des_even.begin(), vec_des_even.end(),
+                test_arr_even[i], &it, CompareNum<int>);
+        assert(ret == kOk);
+        fprintf(stderr, "test_value:%d, value:%d\n", test_arr_even[i], *it);
+    }
+
+    fprintf(stderr, "\n");
+    // ascend search using RandomIterator
+    std::deque<int> deq_asc_odd(arr_asc_odd, arr_asc_odd+sizeof(arr_asc_odd)/sizeof(arr_asc_odd[0]));
+    Print(deq_asc_odd.begin(), deq_asc_odd.end(), deq_asc_odd[0]);
+    std::deque<int>::iterator deq_it;
+    for (int i = (int)(sizeof(test_arr_odd)/sizeof(test_arr_odd[0])) - 1; i >= 0; --i)
+    {
+        Code ret = BinaryAscendSearch(deq_asc_odd.begin(), deq_asc_odd.end(),
+                    test_arr_odd[i], &deq_it, CompareNum<int>);
+        assert(ret == kOk);
+        fprintf(stderr, "test_value:%d, value:%d\n", test_arr_odd[i], *deq_it);
+    }
+    fprintf(stderr, "\n");
+
+    std::deque<int> deq_asc_even(arr_asc_even, arr_asc_even+sizeof(arr_asc_even)/sizeof(arr_asc_even[0]));
+    Print(deq_asc_even.begin(), deq_asc_even.end(), deq_asc_even[0]);
+    for (int i = (int)(sizeof(test_arr_even)/sizeof(test_arr_even[0])) -1; i >= 0; --i)
+    {
+        Code ret = BinaryAscendSearch(deq_asc_even.begin(), deq_asc_even.end(),
+                    test_arr_even[i], &deq_it, CompareNum<int>);
+        assert(ret == kOk);
+        fprintf(stderr, "test_value:%d, value:%d\n", test_arr_even[i], *deq_it);
+    }
+    fprintf(stderr, "\n");
+
+    fprintf(stderr, "\n");
+    // ascend search using RandomIterator and ComparePair
+    std::vector<std::pair<std::string, std::string> > dir;
+    dir.push_back(std::make_pair<std::string, std::string>("aa", "aa.txt"));
+    dir.push_back(std::make_pair<std::string, std::string>("hh", "hh.txt"));
+    dir.push_back(std::make_pair<std::string, std::string>("oo", "oo.txt"));
+    dir.push_back(std::make_pair<std::string, std::string>("uu", "uu.txt"));
+
+    std::pair<std::string, std::string> finds[] = {/*{{{*/
+        std::pair<std::string, std::string>("aa", ""),
+        std::pair<std::string, std::string>("bb", ""),
+        std::pair<std::string, std::string>("cc", ""),
+        std::pair<std::string, std::string>("hh", ""),
+        std::pair<std::string, std::string>("ii", ""),
+        std::pair<std::string, std::string>("jj", ""),
+        std::pair<std::string, std::string>("oo", ""),
+        std::pair<std::string, std::string>("pp", ""),
+        std::pair<std::string, std::string>("qq", ""),
+        std::pair<std::string, std::string>("uu", ""),
+        std::pair<std::string, std::string>("vv", ""),
+        std::pair<std::string, std::string>("xx", ""),
+    };/*}}}*/
+    fprintf(stderr, "finds num:%zu, single size:%zu\n", sizeof(finds)/sizeof(finds[0]), sizeof(finds[0]));
+
+    std::vector<std::pair<std::string, std::string> >::iterator pair_it;
+    for (int i = 0; i < (int)(sizeof(finds)/sizeof(finds[0])); ++i)
+    {
+        Code ret = BinaryAscendSearch(dir.begin(), dir.end(), finds[i], &pair_it,
+                ComparePair<std::pair<std::string, std::string> >);
+        assert(ret == kOk);
+        fprintf(stderr, "finds key:%s, value:(%s, %s)\n", finds[i].first.c_str(), pair_it->first.c_str(), 
+                pair_it->second.c_str());
+    }
+    fprintf(stderr, "\n");
+
 
     return 0;
-}
+}/*}}}*/
 #endif
--- base/search.h
+++ base/search.h
@@ -20,6 +20,23 @@ int CompareNum(const void *first, const void *second)
     return (*(T*)first - *(T*)second);
 }
 
+// std::pair<std::string, std::string>
+template <typename Pair>
+int ComparePair(const void *first, const void *second)
+{
+    return ((Pair*)first)->first.compare(((Pair*)second)->first);
+}
+
+/**
+ * Note: 1. this search is not precisely searching, it's range searching
+ *          for example, an array : {40, 30, 20, 10}, here 20 means [20, 30), if you find 25,
+ *          then it will return 20 for you, as the same, if you find 41, you will get 40;
+ *
+ *       2. This is usefull when it is index infomation, each index points to one data 
+ *          block which has not only one data;
+ *
+ *       3. The array is sorted descending
+ */
 template <typename T>
 Code BinaryDescendSearch(const T *arr, int size, const T &data, int *pos, Compare comp)
 {/*{{{*/
@@ -47,6 +64,9 @@ Code BinaryDescendSearch(const T *arr, int size, const T &data, int *pos, Compar
     return kOk;
 }/*}}}*/
 
+/**
+ * Note: 1. this function is same to BinaryDescendSearch except the array is sorted ascending
+ */
 template <typename T>
 Code BinaryAscendSearch(const T *arr, int size, const T &data, int *pos, Compare comp)
 {/*{{{*/
@@ -72,6 +92,71 @@ Code BinaryAscendSearch(const T *arr, int size, const T &data, int *pos, Compare
 
     *pos = low;
     return kOk;
+}/*}}}*/
+
+template <typename RandomIterator, typename T>
+Code BinaryDescendSearch(RandomIterator first, RandomIterator last, const T &data, RandomIterator *pos, Compare comp)
+{/*{{{*/
+    if (first > last || pos == NULL) return kInvalidParam;
+
+    int low = 0;
+    int mid = 0;
+    int high = last - first - 1;
+
+    while (low < high)
+    {
+        mid = (low+high)/2;
+        if (comp(&(*(first+mid)), &data) > 0)
+            low = mid+1;
+        else if (comp(&(*(first+mid)), &data) < 0)
+            high = mid;
+        else
+        {
+            low = mid;
+            break;
+        }
+    }
+
+    *pos = first + low;
+    return kOk;
+}/*}}}*/
+
+template <typename RandomIterator, typename T>
+Code BinaryAscendSearch(RandomIterator first, RandomIterator last, const T &data, RandomIterator *pos, Compare comp)
+{/*{{{*/
+    if (first > last || pos == NULL) return kInvalidParam;
+
+    int low = 0;
+    int mid = 0;
+    int high = last - first - 1;
+
+    while (low < high)
+    {
+        mid = (low+high+1)/2;
+        if (comp(&(*(first+mid)), &data) > 0)
+            high = mid - 1;
+        else if (comp(&(*(first+mid)), &data) < 0)
+            low = mid;
+        else
+        {
+            low = mid;
+            break;
+        }
+    }
+
+    *pos = first + low;
+    return kOk;
+}/*}}}*/
+
+template <typename RandomIterator>
+void Print(RandomIterator first, RandomIterator last, int type)
+{/*{{{*/
+    fprintf(stderr, "{ ");
+    for (; first != last; ++first)
+    {
+        fprintf(stderr, "%d, ", *first);
+    }
+    fprintf(stderr, " }\n");
 }/*}}}*/
 
 }
