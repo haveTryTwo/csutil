e8601e7bd4756c6a97ef8a6376dc22654a3756b7
刘中超 <liuzhongchao197@163.com>
2019-10-01 03:13:22 +0800

    1)Adding: add pbtojson and jsontopb

---
+++ proto/pb_to_json.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2015 The CSUTIL Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PB_TO_JSON_H_
+#define PB_TO_JSON_H_
+
+#include <string>
+
+#include <stdint.h>
+
+#include "rapidjson/document.h"
+#include <google/protobuf/message.h>
+
+#include "base/status.h"
+
+namespace proto 
+{
+
+base::Code PBToJsonWithOutExtension(const ::google::protobuf::Message &msg, std::string *json);
+base::Code JsonToPBWithOutExtension(const std::string &json, ::google::protobuf::Message *msg);
+
+base::Code PBToJsonWithOutExtension(const ::google::protobuf::Message &msg, rapidjson::Value::AllocatorType &alloc, rapidjson::Value *json);
+base::Code JsonToPBWithOutExtension(const rapidjson::Value &json, ::google::protobuf::Message *msg);
+
+}
+
+#endif
