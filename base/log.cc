9490437ca864638b91b9eb09dab7ddb0c0f9fd6d
haveTryTwo <liuzhongchao197@163.com>
2024-01-28 17:39:00 +0800

    1)Modifying: Adaptive log information length printing

--- base/log.cc
+++ base/log.cc
@@ -15,6 +15,7 @@
 #include <time.h>
 #include <unistd.h>
 
+#include "base/mutex.h"
 #include "base/status.h"
 
 namespace base {
@@ -26,7 +27,10 @@ static char g_log_path[kFilePathLen] = {0};
 static char g_log_path_fmt[kFilePathLen] = {0};
 static int g_log_level = kErrorLevel;
 static FILE *g_log_fp = NULL;
-static pthread_mutex_t g_log_mutex = PTHREAD_MUTEX_INITIALIZER;
+// static pthread_mutex_t g_log_mutex = PTHREAD_MUTEX_INITIALIZER;
+static Mutex g_log_mutex;
+
+static const char *kTooLongMsg = "File name or content too long!";
 
 void SetLogLevel(int level) { /*{{{*/
   if (level >= kDebugLevel || level <= kFatalErrorLevel) {
@@ -54,14 +58,13 @@ void CloseLogStream() { /*{{{*/
 
 void DestroyLog() { /*{{{*/
   CloseLogStream();
-  pthread_mutex_destroy(&g_log_mutex);
+  //  pthread_mutex_destroy(&g_log_mutex);
 } /*}}}*/
 
 void PrintLog(const char *file_path, const char *func, int line_no, int log_level,
               const char *format, ...) { /*{{{*/
   if (log_level < g_log_level || NULL == file_path || NULL == format) return;
 
-  int n = 0;
   int ret = kOk;
   time_t cur;
   struct tm cur_tm = {0};
@@ -71,29 +74,61 @@ void PrintLog(const char *file_path, const char *func, int line_no, int log_leve
   file_name = strrchr(file_path, '/');
   file_name = (file_name == NULL) ? file_path : (file_name + 1);
 
-  pthread_mutex_lock(&g_log_mutex);
+  MutexLock mutex_lock(&g_log_mutex);
+  //  pthread_mutex_lock(&g_log_mutex);
 
   time(&cur);
   localtime_r(&cur, &cur_tm);
 
   ret = CheckLog(&cur_tm);
   if (kOk == ret) {
-    n = snprintf(log_buf, sizeof(log_buf),
-                 "%4d/%02d/%02d %02d:%02d:%02d - [pid: %d][%s][%s %s %d] - ", cur_tm.tm_year + 1900,
-                 cur_tm.tm_mon + 1, cur_tm.tm_mday, cur_tm.tm_hour, cur_tm.tm_min, cur_tm.tm_sec,
-                 getpid(), GetLogLevel(log_level), file_name, func, line_no);
+    int n = snprintf(
+        log_buf, sizeof(log_buf) - 2, "%4d/%02d/%02d %02d:%02d:%02d - [pid: %d][%s][%s %s %d] - ",
+        cur_tm.tm_year + 1900, cur_tm.tm_mon + 1, cur_tm.tm_mday, cur_tm.tm_hour, cur_tm.tm_min,
+        cur_tm.tm_sec, getpid(), GetLogLevel(log_level), file_name, func, line_no);
+    if (n >= (sizeof(log_buf) - 2)) {
+      fwrite(log_buf, sizeof(char), sizeof(log_buf) - 2, g_log_fp);
+      std::string err_msg("; Error message:");
+      err_msg.append(kTooLongMsg);
+      err_msg.append("\n");
+      fwrite(err_msg.c_str(), sizeof(char), err_msg.size(), g_log_fp);
+      fflush(g_log_fp);
+      return;
+    }
 
     va_list arg_ptr;
     va_start(arg_ptr, format);
-    vsnprintf(log_buf + n, sizeof(log_buf) - n - 2, format, arg_ptr);
+    int real_len = vsnprintf(log_buf + n, sizeof(log_buf) - n - 2, format, arg_ptr);
     va_end(arg_ptr);
-    strcat(log_buf, "\n");
+    if (real_len >= (sizeof(log_buf) - n - 2)) {
+      char *extend_buf = new char[real_len + 2];
+      if (extend_buf == NULL) {  // NOTE:htt, just log content
+        strcat(log_buf, "\n");
+        fwrite(log_buf, sizeof(char), sizeof(log_buf) - 1, g_log_fp);
+        fflush(g_log_fp);
+        return;
+      }
 
-    fwrite(log_buf, sizeof(char), strlen(log_buf), g_log_fp);
+      va_list arg_ptr;
+      va_start(arg_ptr, format);
+      vsnprintf(extend_buf, real_len, format, arg_ptr);
+      va_end(arg_ptr);
+
+      fwrite(log_buf, sizeof(char), n, g_log_fp);
+      strcat(extend_buf, "\n");
+      fwrite(extend_buf, sizeof(char), real_len + 1, g_log_fp);
+      fflush(g_log_fp);
+
+      delete[] extend_buf;
+      return;
+    }
+
+    strcat(log_buf, "\n");
+    fwrite(log_buf, sizeof(char), n + real_len + 1, g_log_fp);
     fflush(g_log_fp);
   }
 
-  pthread_mutex_unlock(&g_log_mutex);
+  //  pthread_mutex_unlock(&g_log_mutex);
 } /*}}}*/
 
 static Code CheckLog(const struct tm *cur_tm) { /*{{{*/
