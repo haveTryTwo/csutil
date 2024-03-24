c4b1a8c036aed4386fa3e40ac0565f0723330044
haveTryTwo <liuzhongchao197@163.com>
2024-03-24 21:10:39 +0800

    1)Adding: add pid controller which is a feedback loop controller widely used in industrial control systems

---
+++ base/pid_controller.cc
@@ -0,0 +1,39 @@
+// Copyright (c) 2015 The CSUTIL Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "pid_controller.h"
+
+namespace base {
+
+PidController::PidController(double dt, double Kp, double kd, double ki)
+    : dt_(dt), kp_(Kp), kd_(kd), ki_(ki), pre_error_(0), integral_(0) {}
+
+PidController::~PidController() {}
+
+/**
+ * NOTE:htt, 获取本次新增值
+ */
+double PidController::calculate(double setpoint, double pre_value) {
+  // 计算误差
+  double error = setpoint - pre_value;
+
+  // 比例项
+  double pout = kp_ * error;
+
+  // 积分项
+  integral_ += error * dt_;
+  double iout = ki_ * integral_;
+
+  // 导数项
+  double derivative = (error - pre_error_) / dt_;
+  double dout = kd_ * derivative;
+
+  double output = pout + iout + dout;
+
+  pre_error_ = error;
+
+  return output;
+}
+
+}  // namespace base
---
+++ base/pid_controller.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2015 The CSUTIL Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_PID_CONTROLLER_H_
+#define BASE_PID_CONTROLLER_H_
+
+#include <stdint.h>
+#include <sys/time.h>
+
+#include <map>
+#include <string>
+
+#include "base/common.h"
+#include "base/mutex.h"
+#include "base/status.h"
+
+namespace base {
+
+// NOTE:htt, see: https://gist.github.com/bradley219/5373998
+class PidController {
+ public:
+  PidController(double dt, double kp, double kd, double ki);
+  ~PidController();
+
+  // NOTE:htt, 获取新增值
+  double calculate(double setpoint, double pre_value);
+
+ private:
+  double dt_;         // loop interval time
+  double kp_;         // 比例增益
+  double kd_;         // 微分增益
+  double ki_;         // 积分增益
+  double pre_error_;  // 上一次误差值
+  double integral_;   // 积分项
+};
+
+#endif
+
+}  // namespace base
